f586b1f9908668b27ae35b8bd097afe5458d385d
Kathleen Kelly <murphy.kaite@gmail.com>
2020-05-28 00:55:39 -0700

    added classes

--- Develop/app.js
+++ Develop/app.js
@@ -13,6 +13,73 @@ const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+// ***prompt the user for their email, id, and specific information based on their role with the company. For instance, an intern may provide their school, whereas an engineer may provide their GitHub username.
+
+// Employee is parent class
+  class Employee {
+    constructor(name) {
+      this.name = name;
+      this.email = email;
+    }
+
+    getName() {
+    }
+
+    getId() {
+    }
+
+    getEmail(){
+    }
+
+    getRole() {
+    }
+
+  }
+
+  class Manager {
+    constructor(officeNumber) {
+      this.officeNumber = officeNumber;
+    }
+
+    getRole() {
+      return Manager;
+    }
+
+  }
+
+  class Engineer {
+    constructor() {
+      //GitHub username
+      this.github = github;
+    }
+
+    getGitHub() {}
+
+    getRole() {
+      //Overridden to return 'Engineer' - how do you override something?
+      return Engineer;
+    }
+    
+  }
+
+  class Intern {
+    constructor {
+      this.school = school;
+    }
+
+    getSchool()
+
+    getRole(){
+      //overriden to return 'Intern'
+      return Intern;
+    }
+
+  }
+
+
+
+
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
@@ -27,6 +94,9 @@ const render = require("./lib/htmlRenderer");
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
 // employee type.
+
+# Title:
+${data.title}
 
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
 // and Intern classes should all extend from a class named Employee; see the directions
